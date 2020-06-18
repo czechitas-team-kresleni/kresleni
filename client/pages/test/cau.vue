998cb83dfeed49df46705adc659ed185904f2c0c
Tomas Doslik <tom@MacBook-Pro.local>
2020-06-18 17:05:48 +0200

    bootstrap vue

--- client/pages/test/cau.vue
+++ client/pages/test/cau.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>cau</div>
+  <div>
+    <div>
+      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
+        <b-dropdown-item>First Action</b-dropdown-item>
+        <b-dropdown-item>Second Action</b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item active>Active action</b-dropdown-item>
+        <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+      </b-dropdown>
+    </div>
+    <div class="mt-5">cau</div>
+  </div>
 </template>
 
 <script>
