9b5aee34b5ecf6367cfbbaeafa91799735b4302f
danakal <dana.kalcikova@centrum.cz>
2020-06-20 13:34:59 +0200

    Zadání

---
+++ client/pages/challenges/_id/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <h3>Název</h3>
+
+      <h3>Zadání</h3>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti expedita iusto laudantium accusamus dicta repellendus sit, distinctio cupiditate dolor repudiandae, culpa, aperiam nam esse enim maiores? Cumque ipsa quod et facere modi laborum rerum delectus inventore quisquam enim, eligendi explicabo aspernatur voluptatum vel repellat commodi odio facilis. Excepturi, corporis nam.</p>
+
+      
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/pages/challenges/send/_id.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+      
+  <v-container class="grey lighten-5">
+      
+    <v-row >
+      <v-col 
+      cols="12"
+        sm="6"
+      >
+        <v-card
+          class="pa-3"
+          outlined
+          tile
+        >
+          <h3>Název</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, facilis.</p>
+
+      <h3>Popis</h3>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque reprehenderit harum, saepe beatae ea quo aliquid ratione iusto excepturi alias quod iure molestias id commodi incidunt aperiam mollitia similique, corporis expedita consequuntur consequatur blanditiis! Quod possimus mollitia veniam asperiores voluptatum, explicabo cumque dicta nulla dolores earum voluptatibus maiores ut dolore impedit ipsum ad voluptate, expedita quo nemo error iste aspernatur.</p>
+
+      <h3> Počet bodů</h3>
+
+      <div>
+          3
+        </div>
+        </v-card>
+      </v-col>
+      <v-col 
+      cols="12"
+        sm="6"
+        
+      >
+        <v-card
+          class="pa-3"
+          outlined
+          tile
+        >
+        <div>
+             <h3> Vlastní poznámky:</h3>
+            <v-textarea
+          solo
+          name="input-7-4"
+          label="Sem si můžeš napsat poznámku"
+        ></v-textarea>
+            
+        </div>
+
+        <div>
+        <h3> Nahrát</h3>
+        <v-file-input
+        label="File input"
+        filled
+        prepend-icon="mdi-camera"
+        ></v-file-input>
+         <v-btn block color="grey" dark>Block Button</v-btn>
+       
+
+        </div>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+      
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
