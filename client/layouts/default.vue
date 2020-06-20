6bc0853ffeea31dfb2e41db293fe610afd3088ad
Thu Thuy <61125935+mtthuy22@users.noreply.github.com>
2020-06-20 19:42:39 +0200

    default text

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -11,47 +11,47 @@
       overflow
       color="grey lighten-1"
     >
-    
-  <div> 
-    <p> Vitej zpet, {{user_name}}!</p>
+      <div>
+        <p>Vitej zpet, {{user_name}}!</p>
 
-      <v-avatar size="200px">
-        <img alt="Avatar" src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460" />
-      </v-avatar>
+        <v-avatar size="200px">
+          <img alt="Avatar" src="https://avatars0.githubusercontent.com/u/9064066?v=4&s=460" />
+        </v-avatar>
 
-      <hr />
-      
-   <v-progress-linear
-      background-color="grey darken-1"
-      color="grey darken-3"
-      height="30"
-      value="20"
-    >
-    </v-progress-linear>
- 
-    <hr>
-       
-     <div>
-       {{user_level}} 
-     </div>
+        <hr />
 
-   </div >
-    
-    
+        <v-progress-linear
+          background-color="grey darken-1"
+          color="grey darken-3"
+          height="30"
+          value="20"
+        ></v-progress-linear>
+
+        <hr />
+
+        <div>{{user_level}}</div>
+      </div>
     </v-navigation-drawer>
 
     <v-app-bar :clipped-left="primaryDrawer.clipped" app color="grey darken-3">
       <v-app-bar-nav-icon
         v-if="primaryDrawer.type !== 'permanent'"
         @click.stop="primaryDrawer.model = !primaryDrawer.model"
-        
       ></v-app-bar-nav-icon>
 
       <v-toolbar-items color="grey darken-3">
-        <v-btn  color="grey darken-3" to="/">Domu</v-btn>
-        <v-btn  color="grey darken-3" to="/challenges/send">Dnesni</v-btn>
-        <v-btn  color="grey darken-3" to="/challenges">Galerie</v-btn>
-       <v-btn color="grey darken-3" to='/profil'>Profil</v-btn>
+        <v-btn color="grey darken-3" to="/">
+          <span class="light_text">Domu</span>
+        </v-btn>
+        <v-btn color="grey darken-3" to="/challenges/send">
+          <span class="light_text">Dnesni</span>
+        </v-btn>
+        <v-btn color="grey darken-3" to="/challenges">
+          <span class="light_text">Galerie</span>
+        </v-btn>
+        <v-btn color="grey darken-3" to="/profil">
+          <span class="light_text">Profil</span>
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
@@ -65,8 +65,8 @@
       </v-container>
     </v-main>
 
-    <v-footer :inset="footer.inset" app  color="grey darken-3">
-      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :inset="footer.inset" app color="grey darken-3">
+      <span class="px-4 light_text">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
@@ -93,3 +93,9 @@ export default {
   }
 };
 </script>
+
+<style>
+.light_text {
+  color: #f5f5f5;
+}
+</style>
