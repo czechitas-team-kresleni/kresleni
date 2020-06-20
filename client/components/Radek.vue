2825c674d1537b7b0fb8b9f707596639e530e048
danakal <dana.kalcikova@centrum.cz>
2020-06-20 11:40:15 +0200

    Řádek

---
+++ client/components/Radek.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+      <h3 class="text-left ml-3">Název výzvy</h3>
+       
+  <v-row>
+    <v-col cols="12" >
+      <v-card>
+        <v-container fluid>
+          <v-row>
+            <v-col
+              v-for="n in 6"
+              :key="n"
+              class="d-flex child-flex"
+              cols="2"
+            >
+              <v-card flat tile class="d-flex">
+                <v-img
+                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                  aspect-ratio="1"
+                  class="grey lighten-2"
+                >
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-col>
+  </v-row>
+          
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/pages/challenges/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1> Předchozí výzvy</h1>
+<blockquote class="blockquote">Our constantly changing drawing challenge themes and art prompts are guaranteed to get you outside of your comfort zone while helping you sharpen your skills, gain exposure, and win awesome prizes from our partners. From doodling on coffee cups, to illustrating strangers, explore just how much you can push your creative limits!</blockquote>
+
+<hr>
+<radek></radek>
+
+
+</div>
+  
+</template>
+
+<script>
+import Radek from '../../components/Radek.vue';
+
+export default {
+    
+    name: "challenges",
+    layout: "default",
+    components: {
+        radek: Radek
+    }
+
+}
+</script>
+
+<style>
+
+</style>
