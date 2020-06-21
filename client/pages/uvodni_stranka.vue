285418c79649621c057e46bd19c1457a2567e671
Thu Thuy <61125935+mtthuy22@users.noreply.github.com>
2020-06-21 11:39:39 +0200

    default,id, profil

---
+++ client/pages/uvodni_stranka.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card>
+        <h4>Detail vyzvy</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis voluptatem repellat earum iusto autem quia nam laboriosam tempora accusamus, assumenda libero magni pariatur asperiores necessitatibus animi eaque suscipit voluptas id?</p>
+    </v-card>
+    <v-divider></v-divider>
+    
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
