b81ba39754f2661b1e8cbfaf2e01d0283fcbf516
danakal <dana.kalcikova@centrum.cz>
2020-06-21 11:39:53 +0200

    Výpočty dat

--- client/store/global.js
+++ client/store/global.js
@@ -2,7 +2,8 @@ export const state = () => ({
   games: [],
   kresby: [],
   users: [],
-  levels:[]
+  levels:[],
+  handins:[],
 })
 
 export const getters = {
@@ -21,13 +22,59 @@ export const getters = {
 
       // })
 
-      //((user.exp - previous_threshold) * 100) / (next_threshold - previous_threshold);
+      //moje odevzdane hry
+      let myhandins = state.kresby.filter(handin => handin.user_id === user.id);
+      let finished_games = [];
+      //celkovy pocet lajku za me odevzdane hry
+      let likes = 0;
+      myhandins.forEach(function(handin) {
+          likes += handin.likes;
+          finished_games.push(state.games.find(game => handin.game_id === game.id))
+      })
+      //pocet odevzdanych her
+      user.finished_games = myhandins.length;
+      //pocet neodevzdanych her
+      user.unfinished_games = state.games.length - user.finished_games;
+
+
+      //celkový počet bodů za všechny odevzdané hry
+      let exp = 0;
+      finished_games.forEach(function(game) {
+        exp += game.exp;
+      })
+
+      //přepočet bodů - bonus za likes
+      user.exp = exp;
+      user.exp += likes * 0.5;
+      
+      //nalezení nižších úrovní
+      let levels = state.levels.filter(level => level.threshold <= user.exp);
+
+      //nalezení úrovně uživatele
+      let level = levels.pop();
+      user.level = {...level};
+
+      //nalezení nejbližší vyšší úrovně
+      let next_level = state.levels.find(lvl => lvl.uroven === (level.uroven + 1))
+      if(typeof next_level === 'undefined') {
+        user.level.max = true;
+      } else {
+        //výpočet % postupu v mojí úrovni
+        user.level.progress = ((user.exp - level.threshold) * 100) / (next_level.threshold - level.threshold);
+        user.level.next_threshold = next_level.threshold;
+        user.level.exp_needed = user.level.next_threshold - user.exp;
+      }
+
+      users.push(user)
+
+      //
       //user.procento = nejaky vypocet
     })
     return users;
 
-    return state.users;
   },
+
+
   getLevels:(state) => {
     return state.levels;
   }
