75e56ee91304a653828b0c7776c623bb3c379b3f
Thu Thuy <61125935+mtthuy22@users.noreply.github.com>
2020-06-24 10:49:20 +0200

    pridany odkazy do ikonek

--- client/pages/profil/_id.vue
+++ client/pages/profil/_id.vue
@@ -4,40 +4,42 @@
     <div style="display: flex;
     align-items: center;
     justify-content: space-between;">
-      <blockquote class="blockquote"><p
-        class="m-4">{{user.text}}</p></blockquote>
+      <blockquote class="blockquote">
+        <p class="m-4">{{user.text}}</p>
+      </blockquote>
       <v-avatar size="80px" class="pa-4" tile>
         <img alt="Avatar" v-bind:src="user.foto" />
       </v-avatar>
     </div>
 
     <div class="ml-4 mt-0">
-      <v-icon>{{'mdi-facebook'}}</v-icon>
-      <v-icon>{{'mdi-instagram'}}</v-icon>
-      <v-icon>{{'mdi-artstation'}}</v-icon>
+      <a href="http://www.facebook.com" target="_blank" style="text-decoration:none;"><v-icon>{{'mdi-facebook'}}</v-icon></a>
+      <a href="http://www.instagram.com" target="_blank" style="text-decoration:none;"><v-icon>{{'mdi-instagram'}}</v-icon></a>
+      <a href="http://www.artstation.com" target="_blank" style="text-decoration:none;"><v-icon>{{'mdi-artstation'}}</v-icon></a>
     </div>
+  
 
     <v-divider></v-divider>
     <v-container fluid>
-    <v-row class="mb-6 levels" no-gutters>
-      <v-col v-for=" level in levels" :key="level.id">
-        <v-card
-          class="pa-2 text-center levels-row level_text_size "
-          :class="{level_unlocked:level.uroven <= user.level.uroven, level_locked: level.uroven > user.level.uroven}"
-          tile
-          outlined
-          style="text-transform: uppercase;"
-        >
-          {{level.nazev}}
-          <v-icon v-if="level.uroven > user.level.uroven" style="float: right;">{{ 'mdi-lock'}}</v-icon>
-          <v-icon
-            v-else
-            style="float: right; vertical-align: middle"
-          >{{ 'mdi-lock-open-variant-outline'}}</v-icon>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-row class="mb-6 levels" no-gutters>
+        <v-col v-for=" level in levels" :key="level.id">
+          <v-card
+            class="pa-2 text-center levels-row level_text_size"
+            :class="{level_unlocked:level.uroven <= user.level.uroven, level_locked: level.uroven > user.level.uroven}"
+            tile
+            outlined
+            style="text-transform: uppercase;"
+          >
+            {{level.nazev}}
+            <v-icon v-if="level.uroven > user.level.uroven" style="float: right;">{{ 'mdi-lock'}}</v-icon>
+            <v-icon
+              v-else
+              style="float: right; vertical-align: middle"
+            >{{ 'mdi-lock-open-variant-outline'}}</v-icon>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 
     <v-divider></v-divider>
 
